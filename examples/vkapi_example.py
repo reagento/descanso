75d3baf12e74d0b246c74583a1d90ab861ebf926
Andrey Tikhonov <17@itishka.org>
2025-11-08 22:40:36 +0100

    fix vk example

--- examples/vkapi_example.py
+++ examples/vkapi_example.py
@@ -7,8 +7,9 @@ from typing import List, TypeVar, Generic
 from adaptix import Retort, dumper, Chain
 from requests import Session
 
-from dataclass_rest import get
-from dataclass_rest.http.requests import RequestsClient
+from descanso import get
+from descanso.http.requests import RequestsClient
+from descanso.request_transformers import Query
 
 T = TypeVar("T")
 
@@ -39,23 +40,28 @@ class UsersSearchResult:
 
 class VkClient(RequestsClient):
     def __init__(self, token: str):
-        session = Session()
-        session.params = {"access_token": token, "v": "5.131"}
-        super(VkClient, self).__init__(
-            base_url="https://api.vk.com/method/",
-            session=session,
-        )
-
-    def set_token(self, token: str) -> None:
-        self.session.query_params["access_token"] = token
-
-    def _init_request_args_factory(self) -> Retort:
-        return Retort(recipe=[
+        self.token = token
+        query_dumper = Retort(recipe=[
             dumper(bool, int),
             dumper(int, str),
             dumper(List[int], lambda d: ",".join(d), Chain.LAST),
             dumper(List[str], lambda d: ",".join(d), Chain.LAST),
         ])
+        body_retort = Retort()
+        super(VkClient, self).__init__(
+            base_url="https://api.vk.com/method/",
+            session=Session(),
+            transformers=[
+                Query("access_token", "{self.token}"),
+                Query("v", "5.131"),
+            ],
+            request_params_dumper=query_dumper,
+            request_body_dumper=body_retort,
+            response_body_loader=body_retort,
+        )
+
+    def set_token(self, token: str) -> None:
+        self.token = token
 
     @get("users.get")
     def get_users(self, user_ids: List[str]) -> Response[List[User]]:
