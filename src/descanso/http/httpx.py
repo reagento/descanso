5edff97bc9b5ca496bce37d9261f0b49669e8648
rusel <ruslan.torbeev@yandex.ru>
2025-11-25 08:34:46 +0300

    refactor: Remove unnecessary inheritance and simplify implementation
    1. Remove BaseHttpxClient class in favor of functions
    2. Remove typing.Self for 3.10 compat
    3. Fix thread-safety issue with base_url

--- src/descanso/http/httpx.py
+++ src/descanso/http/httpx.py
@@ -1,16 +1,22 @@
+__all__ = [
+    "AsyncHttpxClient",
+    "HttpxClient",
+    "HttpxResponseWrapper",
+]
+
+import urllib.parse
 from collections.abc import AsyncIterator, Iterator, Sequence
 from contextlib import asynccontextmanager, contextmanager
-from typing import IO, Any, Generic, Self, TypeVar
+from typing import IO, Any
 
-from httpx import URL, QueryParams, Response
 from httpx import AsyncClient as _AsyncClient
 from httpx import Client as _Client
+from httpx import QueryParams, Response
 from kiss_headers import parse_it
 
 from descanso.client import (
     AsyncClient,
     AsyncResponseWrapper,
-    BaseClient,
     SyncClient,
     SyncResponseWrapper,
 )
@@ -21,8 +27,6 @@ from descanso.request import (
     RequestTransformer,
 )
 from descanso.response import ResponseTransformer
-
-_HttpxClientT = TypeVar("_HttpxClientT", _Client, _AsyncClient)
 
 _FileName = str | None
 _FileContent = IO[bytes] | bytes | str
@@ -35,7 +39,7 @@ _HttpxFile = (
 
 
 class HttpxResponseWrapper(SyncResponseWrapper, AsyncResponseWrapper):
-    def __init__(self: Self, response: Response) -> None:
+    def __init__(self, response: Response) -> None:
         self.status_code = response.status_code
         self.status_text = response.reason_phrase
         self.url = str(response.url)
@@ -43,106 +47,105 @@ class HttpxResponseWrapper(SyncResponseWrapper, AsyncResponseWrapper):
         self.body = None
         self._raw_response = response
 
-    def load_body(self: Self) -> None:
+    def load_body(self) -> None:
         self.body = self._raw_response.content
 
-    async def aload_body(self: Self) -> None:
+    async def aload_body(self) -> None:
         self.body = self._raw_response.content
 
 
-class BaseHttpxClient(BaseClient, Generic[_HttpxClientT]):
+def ensure_trailing_slash(url: str) -> str:
+    if url.endswith("/"):
+        return url
+    return url + "/"
+
+
+def to_httpx_query_params(params: KeyValueList[Any]) -> QueryParams:
+    httpx_params = []
+
+    for key, value in params:
+        if value is None:
+            continue
+        httpx_params.append((key, value))
+
+    return QueryParams(httpx_params)
+
+
+def to_httpx_files(files: KeyValueList[FileData]) -> KeyValueList[_HttpxFile]:
+    httpx_files: KeyValueList[_HttpxFile] = []
+
+    for key, file in files:
+        if file.contents is None:
+            continue
+
+        file_payload = (
+            file.filename,
+            file.contents,
+            file.content_type,
+        )
+
+        httpx_files.append((key, file_payload))
+
+    return httpx_files
+
+
+class HttpxClient(SyncClient):
     def __init__(
-        self: Self,
-        session: _HttpxClientT,
-        base_url: str | None = None,
+        self,
+        base_url: str,
+        session: _Client,
         transformers: Sequence[RequestTransformer | ResponseTransformer] = (),
     ) -> None:
         super().__init__(
             transformers=transformers,
         )
 
-        self._base_url = base_url
-        self._session: _HttpxClientT = session
+        self._base_url = ensure_trailing_slash(base_url)
+        self._session = session
 
-    @contextmanager
-    def _override_base_url(self: Self) -> Iterator[None]:
-        old: URL = self._session.base_url
-
-        if self._base_url is not None:
-            self._session.base_url = self._base_url
-
-        yield
-
-        if old != self._session.base_url:
-            self._session.base_url = old
-
-    def _to_httpx_query_params(
-        self: Self,
-        params: KeyValueList[Any],
-    ) -> QueryParams:
-        httpx_params = []
-
-        for key, value in params:
-            if value is None:
-                continue
-            httpx_params.append((key, value))
-
-        return QueryParams(httpx_params)
-
-    def _to_httpx_files(
-        self: Self,
-        files: KeyValueList[FileData],
-    ) -> KeyValueList[_HttpxFile]:
-        httpx_files: KeyValueList[_HttpxFile] = []
-
-        for key, file in files:
-            if file.contents is None:
-                continue
-
-            file_payload = (
-                file.filename,
-                file.contents,
-                file.content_type,
-            )
-
-            httpx_files.append((key, file_payload))
-
-        return httpx_files
-
-
-class HttpxClient(SyncClient, BaseHttpxClient[_Client]):
     @contextmanager
     def send_request(
-        self: Self,
+        self,
         request: HttpRequest,
     ) -> Iterator[SyncResponseWrapper]:
-        with self._override_base_url():
-            response = self._session.request(
-                method=request.method,
-                url=request.url,
-                headers=request.headers.items(),
-                data=request.body,
-                params=self._to_httpx_query_params(request.query_params),
-                files=self._to_httpx_files(request.files),
-            )
+        response = self._session.request(
+            method=request.method,
+            url=urllib.parse.urljoin(self._base_url, request.url),
+            headers=request.headers.items(),
+            data=request.body,
+            params=to_httpx_query_params(request.query_params),
+            files=to_httpx_files(request.files),
+        )
 
         yield HttpxResponseWrapper(response)
 
 
-class AsyncHttpxClient(AsyncClient, BaseHttpxClient[_AsyncClient]):
+class AsyncHttpxClient(AsyncClient):
+    def __init__(
+        self,
+        base_url: str,
+        session: _AsyncClient,
+        transformers: Sequence[RequestTransformer | ResponseTransformer] = (),
+    ) -> None:
+        super().__init__(
+            transformers=transformers,
+        )
+
+        self._base_url = ensure_trailing_slash(base_url)
+        self._session = session
+
     @asynccontextmanager
     async def asend_request(
-        self: Self,
+        self,
         request: HttpRequest,
     ) -> AsyncIterator[AsyncResponseWrapper]:
-        with self._override_base_url():
-            response = await self._session.request(
-                method=request.method,
-                url=request.url,
-                headers=request.headers.items(),
-                data=request.body,
-                params=self._to_httpx_query_params(request.query_params),
-                files=self._to_httpx_files(request.files),
-            )
+        response = await self._session.request(
+            method=request.method,
+            url=urllib.parse.urljoin(self._base_url, request.url),
+            headers=request.headers.items(),
+            data=request.body,
+            params=to_httpx_query_params(request.query_params),
+            files=to_httpx_files(request.files),
+        )
 
         yield HttpxResponseWrapper(response)
