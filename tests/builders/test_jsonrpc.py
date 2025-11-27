9fe6bc43b59970677d9b0690d3eb01cfece0a080
Andrey Tikhonov <17@itishka.org>
2025-11-28 00:57:33 +0100

    rest more params

--- tests/builders/test_jsonrpc.py
+++ tests/builders/test_jsonrpc.py
@@ -8,11 +8,14 @@ from descanso.jsonrpc import (
 )
 from descanso.request_transformers import (
     Body,
+    BodyModelDump,
     JsonDump,
     Method,
+    Skip,
     Url,
 )
-from descanso.response_transformers import ErrorRaiser, JsonLoad
+from descanso.response_transformers import BodyModelLoad, ErrorRaiser, JsonLoad
+from .test_rest import StubConverter
 from .utils import dirty
 
 
@@ -47,8 +50,33 @@ def test_url():
     ]
 
 
-def test_body():
-    jsonrpc = JsonRPCBuilder(body_name="data")
+def id_generator():
+    return ""
+
+
+def test_params():
+    req_additional = Skip("0")
+    resp_additional = ErrorRaiser(codes=[200])
+    request_body_dumper = StubConverter()
+    response_body_loader = StubConverter()
+    request_body_post_dump = Skip("2")
+    error_raiser = ErrorRaiser(codes=[400])
+    response_body_pre_load = ErrorRaiser(codes=[500])
+    json_rpc_error_raiser = JsonRPCErrorRaiser()
+
+    jsonrpc = JsonRPCBuilder(
+        req_additional,
+        resp_additional,
+        body_name="data",
+        http_method="GET",
+        id_generator=id_generator,
+        request_body_dumper=request_body_dumper,
+        response_body_loader=response_body_loader,
+        request_body_post_dump=request_body_post_dump,
+        error_raiser=error_raiser,
+        json_rpc_error_raiser=json_rpc_error_raiser,
+        response_body_pre_load=response_body_pre_load,
+    )
 
     class Api:
         @jsonrpc("methodname")
@@ -59,46 +87,20 @@ def test_body():
     assert Api.do.spec.doc == "Hello"
     assert Api.do.spec.request_transformers == [
         dirty[JsonRPCMethod](method="methodname"),
+        req_additional,
         dirty[Body](arg="data"),
-        dirty[JsonRPCIdGenerator](id_generator=None),
-        dirty[Url](original_template=""),
-        dirty[PackJsonRPC](),
-        dirty[JsonDump](),
-        dirty[Method](method="POST"),
-    ]
-    assert Api.do.spec.response_transformers == [
-        dirty[ErrorRaiser](),
-        dirty[JsonLoad](),
-        dirty[JsonRPCErrorRaiser](),
-        dirty[UnpackJsonRPC](),
-    ]
-
-
-def id_generator():
-    return ""
-
-
-def test_id_gen():
-    jsonrpc = JsonRPCBuilder(id_generator=id_generator)
-
-    class Api:
-        @jsonrpc("methodname")
-        def do(self) -> Model:
-            """Hello"""
-
-    assert Api.do.spec.name == "do"
-    assert Api.do.spec.doc == "Hello"
-    assert Api.do.spec.request_transformers == [
-        dirty[JsonRPCMethod](method="methodname"),
+        dirty[BodyModelDump](dumper=request_body_dumper),
         dirty[JsonRPCIdGenerator](id_generator=id_generator),
         dirty[Url](original_template=""),
         dirty[PackJsonRPC](),
-        dirty[JsonDump](),
-        dirty[Method](method="POST"),
+        request_body_post_dump,
+        dirty[Method](method="GET"),
     ]
     assert Api.do.spec.response_transformers == [
-        dirty[ErrorRaiser](),
-        dirty[JsonLoad](),
-        dirty[JsonRPCErrorRaiser](),
+        resp_additional,
+        error_raiser,
+        response_body_pre_load,
+        json_rpc_error_raiser,
         dirty[UnpackJsonRPC](),
+        dirty[BodyModelLoad](type_hint=Model, loader=response_body_loader),
     ]
